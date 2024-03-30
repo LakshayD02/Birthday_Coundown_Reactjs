9068bf4947c1ffacc0863440da5c47a2b643cec7
LAKSHAY DHOUNDIYAL <104713661+LakshayD02@users.noreply.github.com>
2024-03-30 14:42:00 +0530

    Generate.jsx

--- src/Generate.jsx
+++ src/Generate.jsx
@@ -44,18 +44,18 @@ const Generate = () => {
           required
         >
           <option value='1'>Select Month</option>
-          <option value='1'>January</option>
-          <option value='2'>February</option>
-          <option value='3'>March</option>
-          <option value='4'>April</option>
-          <option value='5'>May</option>
-          <option value='6'>June</option>
-          <option value='7'>July</option>
-          <option value='8'>August</option>
-          <option value='9'>September</option>
-          <option value='10'>October</option>
-          <option value='11'>November</option>
-          <option value='12'>December</option>
+          <option value='2'>January</option>
+          <option value='3'>February</option>
+          <option value='5'>March</option>
+          <option value='6'>April</option>
+          <option value='7'>May</option>
+          <option value='8'>June</option>
+          <option value='9'>July</option>
+          <option value='10'>August</option>
+          <option value='11'>September</option>
+          <option value='12'>October</option>
+          <option value='13'>November</option>
+          <option value='14'>December</option>
         </select>
       </div>
       <button type='submit' className='btn' onClick={() => generateLink()}>
